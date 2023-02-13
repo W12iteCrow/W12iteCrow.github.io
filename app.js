8dd9e1581e1861a14e3b2f4a9865af413f708b31
W12iteCrow <124715632+W12iteCrow@users.noreply.github.com>
2023-02-13 12:40:56 +0600

    Update app.js

--- app.js
+++ app.js
@@ -5,31 +5,6 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-let item = "";
-
-let box1 = document.getElementById("box1");
-let box2 = document.getElementById("box2");
-let box3 = document.getElementById("box3");
-
-box1.addEventListener("click", function(){
-	(tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-
-});
-
-box2.addEventListener("click", function(){
-	(tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-});
-
-box3.addEventListener("click", function(){
-	(tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-});
-
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
