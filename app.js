9a1ca20f1956d58475430695ff275c9f3ab1bf22
W12iteCrow <124715632+W12iteCrow@users.noreply.github.com>
2023-04-19 22:33:33 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
-
 let item = "";
 
 let btn1 = document.getElementById("btn1");
